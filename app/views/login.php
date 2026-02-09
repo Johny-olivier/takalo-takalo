0d63bfc86dc33f8d666b6031f0a9f6d23e74c500
Faneva01 <faneva.raffan@gmail.com>
2026-02-09 15:46:20 +0300

    adding page index.html

---
+++ app/views/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container">
+        <form action="" method="post">
+            <h2>Welcome !</h2>
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email">
+            <label for="pwd_hash">Password:</label>
+            <input type="password" name="pwd_hash" id="pwd_hash">
+            <div class="select_button">
+                <input type="submit" value="Connexion">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
